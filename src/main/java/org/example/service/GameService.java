fbe17e01051cd7d643330386d2e9d028e212aa05
Matteo Santoro <matteosantoro2799@gmail.com>
2024-02-12 19:35:15 +0100

    fix: swagger display minimal info

--- src/main/java/org/example/service/GameService.java
+++ src/main/java/org/example/service/GameService.java
@@ -20,32 +20,32 @@ public class GameService {
     @Operation(summary = "Create new game", method = "POST", operationId = "game/create", //! operationId must be the same as controller
             tags = {
                     "Game"
-            },
+            }
             /*parameters = {
                     @Parameter(in = ParameterIn.PATH, name = "gameID",
                             required = true, description = "The unique ID belonging to the game", schema = @Schema(type = "string"))
             },*/
-            requestBody = @RequestBody(
-                    description = "username and the number of players are required",
-                    required = true,
-                    content = @Content(
-                            mediaType = "application/json",
-                            encoding = @Encoding(contentType = "application/json"),
-                            schema = @Schema(implementation = CreateGameBody.class)
-                    )
-            ),
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "OK",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    encoding = @Encoding(contentType = "application/json"),
-                                    schema = @Schema(name = "gamee",
-                                            implementation = CreateGameBody.class)
-                            )
-                    ),
-                    @ApiResponse(responseCode = "404", description = "game not found."),
-                    @ApiResponse(responseCode = "500", description = "Internal Server Error.")
-            }
+        //     requestBody = @RequestBody(
+        //             description = "username and the number of players are required",
+        //             required = true,
+        //             content = @Content(
+        //                     mediaType = "application/json",
+        //                     encoding = @Encoding(contentType = "application/json"),
+        //                     schema = @Schema(implementation = CreateGameBody.class)
+        //             )
+        //     ),
+        //     responses = {
+        //             @ApiResponse(responseCode = "200", description = "OK",
+        //                     content = @Content(
+        //                             mediaType = "application/json",
+        //                             encoding = @Encoding(contentType = "application/json"),
+        //                             schema = @Schema(name = "gamee",
+        //                                     implementation = CreateGameBody.class)
+        //                     )
+        //             ),
+        //             @ApiResponse(responseCode = "404", description = "game not found."),
+        //             @ApiResponse(responseCode = "500", description = "Internal Server Error.")
+        //     }
     )
     public String createGame(RoutingContext context) {
         System.out.println("username = " );
